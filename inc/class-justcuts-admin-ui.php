e66634c357f11ee6491ff2fe8b59ed264975cfba
Ryan Hellyer <ryanhellyer@gmail.com>
2019-03-19 01:58:21 +0100

    Implementation of taxonomies and some extra meta

--- inc/class-justcuts-admin-ui.php
+++ inc/class-justcuts-admin-ui.php
@@ -16,6 +16,12 @@ class JustCuts_Admin_UI extends JustCuts_Setup {
 		add_action( 'cmb2_admin_init', array( $this, 'image_metaboxes' ) );
 		add_action( 'cmb2_admin_init', array( $this, 'contact_metaboxes' ) );
 		add_action( 'cmb2_admin_init', array( $this, 'price_metaboxes' ) );
+
+		add_action( 'add_meta_boxes', array( $this, 'add_country_metabox' ) );
+		add_action( 'save_post',      array( $this, 'country_save' ), 10, 2 );
+
+		add_action( 'add_meta_boxes', array( $this, 'add_city_metabox' ) );
+		add_action( 'save_post',      array( $this, 'city_save' ), 10, 2 );
 	}
 
 	/**
@@ -66,24 +72,6 @@ class JustCuts_Admin_UI extends JustCuts_Setup {
 			'sanitization_cb' => 'absint',
 		) );
 
-		$cmb->add_field( array(
-			'name'             => esc_html__( 'Country', 'justcuts' ),
-			'id'               => 'country',
-			'type'             => 'select',
-			'options_cb'       => array( $this, 'countries' ),
-			'escape_cb'        => 'esc_html',
-			'sanitization_cb'  => 'esc_html',
-		) );
-
-		$cmb->add_field( array(
-			'name'             => esc_html__( 'City', 'justcuts' ),
-			'id'               => 'city',
-			'type'             => 'text',
-			'options_cb'       => 'city',
-			'escape_cb'        => 'esc_html',
-			'sanitization_cb'  => 'esc_html',
-		) );
-
 	}
 
 	/**
@@ -107,11 +95,11 @@ class JustCuts_Admin_UI extends JustCuts_Setup {
 			// 'query_args' => array( 'type' => 'image' ), // Only images attachment
 			// Optional, override default text strings
 			'text'             => array(
-				'add_upload_files_text' => 'Replacement', // default: "Add or Upload Files"
-				'remove_image_text'     => 'Replacement', // default: "Remove Image"
-				'file_text'             => 'Replacement', // default: "File:"
-				'file_download_text'    => 'Replacement', // default: "Download"
-				'remove_text'           => 'Replacement', // default: "Remove"
+				'add_upload_files_text' => esc_html__( 'Add or Upload Files', 'justcuts' ),
+				'remove_image_text'     => esc_html__( 'Remove Image', 'justcuts' ),
+				'file_text'             => esc_html__( 'File', 'justcuts' ),
+				'file_download_text'    => esc_html__( 'Download', 'justcuts' ),
+				'remove_text'           => esc_html__( 'Remove', 'justcuts' ),
 			),
 		) );
 
@@ -186,6 +174,185 @@ class JustCuts_Admin_UI extends JustCuts_Setup {
 			'escape_cb'       => 'absint',
 			'sanitization_cb' => 'absint',
 		) );
+
+	}
+
+	/**
+	 * Add country metabox.
+	 */
+	public function add_country_metabox() {
+
+		add_meta_box(
+			'country', // ID
+			__( 'Country', 'justcuts' ), // Title
+			array(
+				$this,
+				'country_metabox', // Callback to method to display HTML
+			),
+			'business', // Post type
+			'side', // Context, choose between 'normal', 'advanced', or 'side'
+			'low'  // Position, choose between 'high', 'core', 'default' or 'low'
+		);
+
+	}
+
+	/**
+	 * Output the country meta box.
+	 */
+	public function country_metabox() {
+
+		?>
+
+		<p>
+			<label for="_country"><strong><?php _e( 'Select a country', 'justcuts' ); ?></strong></label>
+			<br />
+			<select name="_country" id="_country"><?php
+
+			foreach ( $this->countries() as $code => $country ) {
+				$term = $this->get_country_code( get_the_ID() );
+
+				echo '<option ' . selected( $code, $term ) . ' value="' . esc_attr( $code ) . '">' . esc_html( $country ) . '</option>';
+			}
+
+			?>
+			</select>
+			<input type="hidden" id="country-nonce" name="country-nonce" value="<?php echo esc_attr( wp_create_nonce( __FILE__ ) ); ?>">
+		</p><?php
+	}
+
+	private function get_country_code( $post_id ) {
+
+		$terms = wp_get_post_terms( $post_id, 'country', array() );
+		$term = '';
+		if ( isset( $terms[0]->slug ) ) {
+			$term = $terms[0]->slug;
+		}
+
+		return $term;
+	}
+
+	private function get_country_id( $post_id ) {
+
+		$terms = wp_get_post_terms( $post_id, 'country', array() );
+		$term_id = '';
+		if ( isset( $terms[0]->term_id ) ) {
+			$term_id = $terms[0]->term_id;
+		}
+
+		return $term_id;
+	}
+
+	/**
+	 * Save opening times meta box data.
+	 *
+	 * @param  int     $post_id  The post ID
+	 * @param  object  $post     The post object
+	 */
+	public function country_save( $post_id, $post ) {
+
+		// Only save if correct post data sent
+		if ( isset( $_POST['_country'] ) ) {
+
+			// Do nonce security check
+			if ( ! wp_verify_nonce( $_POST['country-nonce'], __FILE__ ) ) {
+				return;
+			}
+
+			// Sanitize and store the data
+			$_country = wp_kses_post( $_POST['_country'] );
+
+			wp_set_post_terms( $post_id, $_country, 'country', false );
+
+			// Set term name
+			$term_id = $this->get_country_id( $post_id );
+			wp_update_term(
+				$term_id,
+				'country',
+				array(
+					'name' => 'Non Catégorisé',
+					'slug' => 'non-categorise'
+				)
+			);
+
+		}
+
+	}
+
+
+
+
+
+
+
+
+	/**
+	 * Add city metabox.
+	 */
+	public function add_city_metabox() {
+
+		add_meta_box(
+			'city', // ID
+			__( 'City', 'justcuts' ), // Title
+			array(
+				$this,
+				'city_metabox', // Callback to method to display HTML
+			),
+			'business', // Post type
+			'side', // Context, choose between 'normal', 'advanced', or 'side'
+			'low'  // Position, choose between 'high', 'core', 'default' or 'low'
+		);
+
+	}
+
+	/**
+	 * Output the city meta box.
+	 */
+	public function city_metabox() {
+
+		$term = $this->get_city_name( get_the_ID() );
+		?>
+
+		<p>
+			<label for="_city"><strong><?php _e( 'Enter the city', 'justcuts' ); ?></strong></label>
+			<br />
+			<input type="text" name="_city" id="_city" value="<?php echo esc_attr( $term ); ?>" />
+			<input type="hidden" id="city-nonce" name="city-nonce" value="<?php echo esc_attr( wp_create_nonce( __FILE__ ) ); ?>">
+		</p><?php
+	}
+
+	private function get_city_name( $post_id ) {
+
+		$terms = wp_get_post_terms( $post_id, 'city', array() );
+		$term = '';
+		if ( isset( $terms[0]->name ) ) {
+			$term = $terms[0]->name;
+		}
+
+		return $term;
+	}
+
+	/**
+	 * Save opening times meta box data.
+	 *
+	 * @param  int     $post_id  The post ID
+	 * @param  object  $post     The post object
+	 */
+	public function city_save( $post_id, $post ) {
+
+		// Only save if correct post data sent
+		if ( isset( $_POST['_city'] ) ) {
+
+			// Do nonce security check
+			if ( ! wp_verify_nonce( $_POST['city-nonce'], __FILE__ ) ) {
+				return;
+			}
+
+			// Sanitize and store the data
+			$_city = wp_kses_post( $_POST['_city'] );
+
+			wp_set_post_terms( $post_id, $_city, 'city', false );
+
+		}
 
 	}
 
